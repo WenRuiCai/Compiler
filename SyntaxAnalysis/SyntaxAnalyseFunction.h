0e130863c4f81d703068ee28a5fa4aedadc3f896
蔡文锐 <17373168@buaa.edu.cn>
2019-10-30 14:18:07 +0800

    nearly finished

--- SyntaxAnalysis/SyntaxAnalyseFunction.h
+++ SyntaxAnalysis/SyntaxAnalyseFunction.h
@@ -113,7 +113,7 @@ void Const_Define(vector<SINGLE_WORD>& Words, int& PointNum, ofstream& output) {
             if (WORD_TYPE == "CHARCON") {
                 PRINT_WORD_AND_ADDPOINT;
             } else if (!Number(Words, PointNum, output)) {
-                symbolTable.addAssignValueError(LINE);
+                symbolTable.addAssignValueError(PRE_WORD_LINE);
             }
             if (WORD_TYPE == "SEMICN") {
                 cout << "<常量定义>" << endl;
